70d6727d18fe8e5e8c8e82fb8cd5e7c1711e7438
lottaku <74823153+lottaku@users.noreply.github.com>
2020-11-27 12:58:26 +0100

    PW

--- build_website.R
+++ build_website.R
@@ -3,7 +3,7 @@
 ##########################################################################
 ##### DON'T PUSH THIS SECTION TO GITHUB  - REMOVE THIS AFTER RUNNING #####
 ##########################################################################
-# Sys.setenv(LAB_KEY = "Your-token")
+#Sys.setenv(LAB_KEY = "dsws21")
 
 # Step 1: Build Website ---------------------------------------------------
 rmarkdown::render_site(encoding = 'UTF-8')
@@ -15,7 +15,8 @@ rmarkdown::render_site(encoding = 'UTF-8')
 ###### THIS WILL ONLY WORK IF YOU HAVE STATICRYPT INSTALLED VIA NPM ######
 ##########################################################################
 
-system( paste0("staticrypt ./docs/journal.html ", Sys.getenv("LAB_KEY")) )
+#system( paste0("staticrypt ./docs/journal.html ", Sys.getenv("LAB_KEY")) )
+system( paste0("staticrypt ./docs/journal.html ", LAB_KEY) )
 
 # Remove and rename resulting files
 file.remove("./docs/journal.html")
